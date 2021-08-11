861ff0e0b76a69ad31c8cae79494a86de75e792a
Harmen Stoppels <harmenstoppels@gmail.com>
2021-08-11 11:54:49 +0200

    Handle GLOB_NOMATCH

--- src/glob.cpp
+++ src/glob.cpp
@@ -14,16 +14,21 @@ std::vector<fs::path> glob_wrapper(std::string const &pattern) {
     memset(&glob_result, 0, sizeof(glob_result));
 
     int return_value = glob(pattern.c_str(), 0, NULL, &glob_result);
-    if (return_value != 0) {
+
+    // Handle errors
+    if (return_value != 0 && return_value != GLOB_NOMATCH) {
         globfree(&glob_result);
         std::stringstream ss;
-        ss << "glob() failed with return_value " << return_value << '\n';
+        ss << "glob() = " << return_value << '\n';
         throw std::runtime_error(ss.str());
     }
 
+    // Collects matches
     std::vector<fs::path> filenames;
-    for(size_t i = 0; i < glob_result.gl_pathc; ++i) {
-        filenames.emplace_back(glob_result.gl_pathv[i]);
+    if (return_value != GLOB_NOMATCH) {
+        for(size_t i = 0; i < glob_result.gl_pathc; ++i) {
+            filenames.emplace_back(glob_result.gl_pathv[i]);
+        }
     }
 
     globfree(&glob_result);
